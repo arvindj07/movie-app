596acdde8d914a151cb3b9945b0bb794a711ad5d
arvindj97 <arvindj97@gmail.com>
2021-02-15 17:49:52 +0530

    Create Store using Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers'
 
+// creating the Redux Store and Passing 'movies' Reducer as argument
+const store= createStore(movies); // Here, the Reducers are passed as arguments
+
+console.log("store: ",store);
 
 ReactDOM.render(
   <React.StrictMode>
