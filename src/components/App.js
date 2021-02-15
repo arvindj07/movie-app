5ccb45f8fa92756e7cfda5181a3380857ba514b4
arvindj97 <arvindj97@gmail.com>
2021-02-15 23:38:21 +0530

    Subscribe to the Store

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,55 @@
 import React from 'react';
-import {data} from '../data';
+// import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { data } from '../data';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="main">
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favorites</div>
-        </div>
+class App extends React.Component {
+  
+  componentDidMount(){
+    const { store }= this.props;
 
-        <div className="list">
-          {/* {data.map((movie)=>{
-            return <MovieCard movie={movie} />      //MovieCard Can also be rendered like this
-          })} */}
-          {data.map((movie,index)=>(
-            <MovieCard movie={movie} key={`movies-${index}`} />
-          ))}
+    store.subscribe(()=>{   //2. Subscribe takes place after Dispatch, and the Store is Updated
+      console.log('Updated');
+      this.forceUpdate();   //3. Forces to Re-render the Component, so that the Updated Store is visible on Browser
+    });
+    //make api call, for now we r not using api to fetch movies, we will be using 'data'
+
+    //dispatch action
+    store.dispatch({    // 1. First Dispatch action takes place, then Subscribe
+      type:'ADD_MOVIES',
+      movies:data,
+    });
+
+    console.log('Store: ',store.getState());
+  }
+
+  render(){
+    const movies= this.props.store.getState();
+    console.log('Render');
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favorites</div>
+          </div>
+
+          <div className="list">
+            {/* {data.map((movie)=>{
+              return <MovieCard movie={movie} />      //MovieCard Can also be rendered like this
+            })} */}
+            {movies.map((movie,index)=>(
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+    
+  }
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -10,22 +10,22 @@ import movies from './reducers'
 const store= createStore(movies); // Here, the Reducers are passed as arguments
 
 console.log("store: ",store);
-console.log("Before-State",store.getState());
+// console.log("Before-State",store.getState()); //getState() gives the State in the Store
 
 //Dispatching the Action by Store, 
 //and this Action object is passed to the Reducer(ie, movies. coz its(movies) the argument that we passed to createStore )
-store.dispatch({
-  type:'ADD_MOVIES',
-  movies:[{name:'Superman'}]
-})
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name:'Superman'}]
+// })
 
 //when the Action is passed to Reducer, the Reducer will return a New-State to the Store and merges it with the curr state
 //The updated state can be Read using store.getState()
-console.log("After-State",store.getState());
+// console.log("After-State",store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
